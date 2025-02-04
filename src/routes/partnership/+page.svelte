8148680f10007b6a24eb14d351130f71ef19724a
Artistizen <artistizen@artistizen.com>
2025-02-04 15:38:33 +0100

    main menu margin fix

--- src/routes/partnership/+page.svelte
+++ src/routes/partnership/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center h-screen">
 	<div class="">
 		<p class="font-light text-7xl">PARTNERS</p>
-		<p class="font-light text-1xl">Arducam. And you?</p>
+		<p class="font-light text-1xl">Arducam - UKR</p>
 	</div>
 </div>
