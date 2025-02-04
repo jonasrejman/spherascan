3d1284cac50032a4760ba72d086f8ba3f87b09b0
Artistizen <artistizen@artistizen.com>
2025-02-04 22:59:40 +0100

    update

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -2,9 +2,9 @@
 	<div class="">
 		<p class="font-light text-7xl">JONAS REJMAN</p>
 		<p class="font-medium text-1xl ml-2">MA, cand. med.</p>
-		<p class="font-light text-1xl ml-2 mb-5">inventor - engineering - image pipeline</p>
+		<p class="font-light text-1xl ml-2 mb-5">lead engineering - development</p>
 		<p class="font-light text-7xl">MICHAEL RUMPOLD</p>
 		<p class="font-medium text-1xl ml-2">MS</p>
-		<p class="font-light text-1xl ml-2">engineering - software</p>
+		<p class="font-light text-1xl ml-2">engineering - software integration</p>
 	</div>
 </div>
