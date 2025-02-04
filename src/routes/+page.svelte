03af136430cd82635669ce10be581f093e3f4a69
Artistizen <artistizen@artistizen.com>
2025-02-04 15:39:59 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center h-screen">
 	<div class="">
 		<p class="font-light text-7xl">SPHERASCAN</p>
-		<p class="font-light text-1xl">photogrammetric hand scanner for medical professionals</p>
+		<p class="font-light text-1xl">photogrammetric 3D hand scanner for medical professionals</p>
 	</div>
 </div>
