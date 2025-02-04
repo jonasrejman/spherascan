b1d993c91c2e905fcbdecbc14a47ce5679d00dd5
Artistizen <artistizen@artistizen.com>
2025-02-04 16:40:33 +0100

    fix

--- src/routes/specs/+page.svelte
+++ src/routes/specs/+page.svelte
@@ -1,11 +1,10 @@
 <div class="flex items-center justify-center h-screen">
 	<div class="">
-		<p class="font-light text-7xl">SPECS</p>
-		<p class="font-light text-1xl mb-5">fastest scanner of human hands</p>
+		<p class="font-light text-7xl mb-5">SPECS</p>
+		<p class="font-light text-1xl">clinic-optimised user experience</p>
 		<p class="font-light text-1xl">32x64MP image resolution</p>
 		<p class="font-light text-1xl">1 second per scan</p>
 		<p class="font-light text-1xl">90 seconds from scan to 3D object</p>
-		<p class="font-light text-1xl">all 3D formats supported</p>
-		<p class="font-light text-1xl">clinic-optimised user experience</p>
+		<p class="font-light text-1xl">open image pipeline integration</p>
 	</div>
 </div>
