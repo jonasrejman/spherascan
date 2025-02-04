352dacc63da3d04ce1742a6aa6a5d406e03d9c15
Artistizen <artistizen@artistizen.com>
2025-02-04 15:29:31 +0100

    people update

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,6 +1,10 @@
 <div class="flex items-center justify-center h-screen">
 	<div class="">
-		<p class="font-light text-7xl">TEAM</p>
-		<p class="font-light text-1xl">People behind Spherascan.</p>
+		<p class="font-light text-7xl">JONAS REJMAN</p>
+		<p class="font-medium text-1xl ml-2">MA, cand. med.</p>
+		<p class="font-light text-1xl ml-2 mb-5">inventor - engineering - image pipeline</p>
+		<p class="font-light text-7xl">MICHAEL RUMPOLD</p>
+		<p class="font-medium text-1xl ml-2">MS</p>
+		<p class="font-light text-1xl ml-2">engineering - software</p>
 	</div>
 </div>
